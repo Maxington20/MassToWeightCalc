3bb194c1c585c04046626b44cb703f0a597b929f
Max Herrington <max.herrington@gmail.com>
2018-01-09 21:14:57 -0500

    Initial Commit to Github

--- MassToWeightCalc/Form1.Designer.cs
+++ MassToWeightCalc/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace MassToWeightCalc
 {
-	partial class Form1
+	partial class KilosToNewtons
 	{
 		/// <summary>
 		/// Required designer variable.
@@ -28,12 +28,124 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(KilosToNewtons));
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.massEnterTextbox = new System.Windows.Forms.TextBox();
+			this.weightConvertTextbox = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(296, 302);
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Cursor = System.Windows.Forms.Cursors.SizeNS;
+			this.label1.Font = new System.Drawing.Font("Segoe UI Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(324, 33);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(343, 63);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "This program allows you to type in a mass \r\nin kilograms and converts it into a w" +
+    "eight \r\nin newtons";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// massEnterTextbox
+			// 
+			this.massEnterTextbox.Font = new System.Drawing.Font("Segoe UI Symbol", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.massEnterTextbox.ImeMode = System.Windows.Forms.ImeMode.Disable;
+			this.massEnterTextbox.Location = new System.Drawing.Point(328, 173);
+			this.massEnterTextbox.Name = "massEnterTextbox";
+			this.massEnterTextbox.Size = new System.Drawing.Size(135, 29);
+			this.massEnterTextbox.TabIndex = 2;
+			this.massEnterTextbox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+			this.massEnterTextbox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.massEnterTextox_KeyPress);
+			// 
+			// weightConvertTextbox
+			// 
+			this.weightConvertTextbox.Cursor = System.Windows.Forms.Cursors.No;
+			this.weightConvertTextbox.Font = new System.Drawing.Font("Segoe UI Symbol", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.weightConvertTextbox.ImeMode = System.Windows.Forms.ImeMode.Off;
+			this.weightConvertTextbox.Location = new System.Drawing.Point(532, 173);
+			this.weightConvertTextbox.Name = "weightConvertTextbox";
+			this.weightConvertTextbox.ReadOnly = true;
+			this.weightConvertTextbox.Size = new System.Drawing.Size(135, 29);
+			this.weightConvertTextbox.TabIndex = 3;
+			this.weightConvertTextbox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.Color.RoyalBlue;
+			this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Blue;
+			this.button1.FlatAppearance.BorderSize = 2;
+			this.button1.Font = new System.Drawing.Font("Segoe UI Symbol", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.Location = new System.Drawing.Point(328, 251);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(135, 35);
+			this.button1.TabIndex = 4;
+			this.button1.Text = "Clear";
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.BackColor = System.Drawing.Color.RoyalBlue;
+			this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.button2.FlatAppearance.BorderColor = System.Drawing.Color.Blue;
+			this.button2.FlatAppearance.BorderSize = 2;
+			this.button2.Font = new System.Drawing.Font("Segoe UI Symbol", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button2.Location = new System.Drawing.Point(532, 251);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(135, 35);
+			this.button2.TabIndex = 5;
+			this.button2.Text = "Convert";
+			this.button2.UseVisualStyleBackColor = false;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// KilosToNewtons
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Text = "Form1";
+			this.BackColor = System.Drawing.Color.DeepSkyBlue;
+			this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+			this.ClientSize = new System.Drawing.Size(693, 326);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.weightConvertTextbox);
+			this.Controls.Add(this.massEnterTextbox);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.pictureBox1);
+			this.Cursor = System.Windows.Forms.Cursors.Default;
+			this.DoubleBuffered = true;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "KilosToNewtons";
+			this.Text = "Kilograms to Newtons";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox massEnterTextbox;
+		private System.Windows.Forms.TextBox weightConvertTextbox;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
 	}
 }
 
--- MassToWeightCalc/Form1.cs
+++ MassToWeightCalc/Form1.cs
@@ -10,11 +10,73 @@ using System.Windows.Forms;
 
 namespace MassToWeightCalc
 {
-	public partial class Form1 : Form
+	public partial class KilosToNewtons : Form
 	{
-		public Form1()
+		public KilosToNewtons()
 		{
 			InitializeComponent();
+
+			//start with both buttons not enabled
+			button1.Enabled = false;
+			button2.Enabled = false;
+			
+		}
+
+		private void massEnterTextox_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			//only allow numbers input from the user
+			if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+		(e.KeyChar != '.'))
+			{
+				e.Handled = true;
+				MessageBox.Show("Only numbers may be used");
+			}
+			
+
+			// only allow one decimal point
+			if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+			{
+				e.Handled = true;
+				MessageBox.Show("Only one decimal may be used");
+			}
+
+			//Half works. It will only enable these when 2 digits or more are entered
+				//for some reason.
+			button1.Enabled = !string.IsNullOrEmpty(massEnterTextbox.Text);
+			button2.Enabled = !string.IsNullOrEmpty(massEnterTextbox.Text);			
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			massEnterTextbox.Text = "";
+			weightConvertTextbox.Text = "";
+			button1.Enabled = false;
+			button2.Enabled = false;
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+
+			double weight = Convert.ToDouble(massEnterTextbox.Text) * 9.8;
+
+			if (weight > 1000)
+			{
+				weightConvertTextbox.Text = "This is too heavy";
+			}
+
+			else if (weight < 10)
+			{
+				weightConvertTextbox.Text = "This is too light";
+			}
+			
+			else
+			{
+				string weightN = Convert.ToString(weight);
+				weightConvertTextbox.Text = weightN;
+			}
+
+			button1.Enabled = true;
+			button2.Enabled = false;
 		}
 	}
 }
